9096256902555e67aaf7f74e6af0c6c030d1ec99
huang <hsheh@qq.com>
2024-12-19 16:05:49 +0800

    Initial commit with existing files

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import { Heading, HStack, Image, Text, VStack, Box } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <Box bg="white"  borderRadius="md">
+    <VStack align="start" >
+      <Image src={imageSrc} alt="Placeholder Image" objectFit="cover" rounded="md"/>
+      <Heading as="h2" size='md' color="black">{title}</Heading>
+      <Text color="gray">{description}</Text>
+      <HStack><Text color="black">See more</Text><FontAwesomeIcon icon={faArrowRight} size="1x" color="black"/></HStack>
+    </VStack>
+    </Box>
+  );
+};
+
+export default Card;
